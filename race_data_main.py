c9578230eb6403e93665434efa9915836fabcd66
RogerMooreIsTheBestBond <tim.goldney@gmail.com>
2013-12-22 08:00:57 +1030

    Update race_data_main.py
    change to have postgres specific include file

--- race_data_main.py
+++ race_data_main.py
@@ -17,6 +17,13 @@ import re
 # example: page = "http://risa.com.au/FreeFields/Results.aspx?Key=2013Nov28,NSW,Wyong"
 
 
+# database connection string.
+# will probably need to change depending on what db engine we're using
+from race_data_postgres import *
+dbconnstr = 'dbname=etltest user=etl password=etlpass'
+
+
+
 # function definitions. prob put these in a separate library file at some stage
 
 def getRaceLocationAndDate(pageURL, state):
@@ -35,15 +42,14 @@ def getRaceLocationAndDate(pageURL, state):
         meetTrial = 1
     return [meetYear, meetMonth, meetDay, meetState, meetLocation, meetTrial]
 
-def needToGetRaceMeet(pageURL, state):
+def needToGetRaceMeet(pageURL, state, dbconnstr):
     # get details about the race meet
     # then check the database to see if we've got data for that meet yet
-    meetDetails = getRaceLocationAndDate(pageURL, state)
-    # TODO: check db to see if we've got this race meet yet
+    meet_details = getRaceLocationAndDate(pageURL, state)
     # return 1 if we need to get it, else 0
-    return 1
+    return checkMeetExists(meet_details, dbconnstr)
 
-def getRaceMeetDetails(soup, meetDetails):
+def getRaceMeetDetails(soup, meetDetails, dbconnstr):
     # save to db details of the race meet;
     # location, date, rail position, weather, penetrometer(!) etc
     header_bottom_div = soup.find('div', class_='race-venue-bottom')
@@ -60,25 +66,18 @@ def getRaceMeetDetails(soup, meetDetails):
     header_comments = soup.find('div', class_='comments').text
 
     # Save these details in the db
-    print('Year: ' + meetDetails[0])
-    print('Month: ' + meetDetails[1])
-    print('Day: ' + meetDetails[2])
-    print('State: ' + meetDetails[3])
-    print('Location: ' + meetDetails[4])
-    print('IsTrial?: ' + str(meetDetails[5]))
-    print('Rail Position: ' + hdr1_elements[0].strip())
-    print('Track Condition: ' + hdr1_elements[1].strip())
-    print('Track Type: ' + hdr1_elements[2].strip())
-    print('Weather: ' + hdr2_elements[0].strip())
-    print('Penetrometer: ' + hdr2_elements[1].strip())
-    print('Results Last Published: ' + hdr2_elements[2].strip())
-    print('Comments: ' + header_comments.strip())
-    return 1
+    return saveMeetDetails([meetDetails[0], meetDetails[1], meetDetails[2], meetDetails[3],
+                                meetDetails[4], str(meetDetails[5]), hdr1_elements[0].strip(),
+                                hdr1_elements[1].strip(), hdr1_elements[2].strip(),
+                                hdr2_elements[0].strip(), hdr2_elements[1].strip(),
+                                hdr2_elements[2].strip(), header_comments.strip()], dbconnstr)
+
+    
 
 def race_name_tag(tag):
     return tag.has_attr('name') and tag.name == 'a'
 
-def getRaceDetails(race_header):
+def getRaceDetails(race_header, meet_id, dbconnstr):
     # save to db details of an individual race
     race_name = race_header.find(race_name_tag).text
     race_name = race_name.replace(' - ', '|')
@@ -96,23 +95,23 @@ def getRaceDetails(race_header):
     race_details = race_details.replace('Official Comments:', '|')
     race_details_elements = race_details.split('|')
 
-    # save these details in db
-    print('Race Number: ' + race_name_elements[0].strip())
-    print('Race Time: ' + race_name_elements[1].strip())
-    print('Race Name: ' + race_name_elements[2].strip())
-    print('Race Distance: ' + race_name_elements[3].strip())
-    print('Race Details: ' + race_details_elements[0].strip())
-    print('Track Condition: ' + race_details_elements[1].strip())
-    print('Winning Time: ' + race_details_elements[2].strip())
+    last_split_time = ''
+    official_comments = ''
     if len(race_details_elements) > 3:
-        print ('Last Split Time: ' + race_details_elements[3].strip())
+        last_split_time = race_details_elements[3].strip()
     if len(race_details_elements) > 4:
-        print ('Official Comments: ' + race_details_elements[4].strip())
+        official_comments = race_details_elements[4].strip()
+    
+    # save these details in db
+    return saveRaceDetails([race_name_elements[0].strip(), race_name_elements[1].strip(),
+                                race_name_elements[2].strip(), race_name_elements[3].strip(),
+                                race_details_elements[0].strip(), race_details_elements[1].strip(),
+                                race_details_elements[2].strip(), last_split_time,
+                                official_comments], meet_id, dbconnstr)
 
-    # return Race Number for reference when getting runners
-    return race_name_elements[0].strip()
 
-def getRaceRunnerDetails(race_runner, race_number):
+
+def getRunnerDetails(race_runner, race_id, dbconnstr):
     # save to db details of an individual runner in a race
     # if we've got a 'class' attribute, we're looking at a runner row.
     if race_runner.has_attr('class'):
@@ -138,32 +137,24 @@ def getRaceRunnerDetails(race_runner, race_number):
         td_tag = td_tag.next_sibling.next_sibling
         starting_price = td_tag.text
 
-        # save to db
-        print('Finish Position: ' + finish_position)
-        print('Number: ' + runner_number)
-        print('Runner Name: ' + runner_name)
-        print('Trainer Name: ' + trainer_name)
-        print('Jockey Name: ' + jockey_name)
-        print('Margin to Winner: ' + margin_to_winner)
-        print('Barrier: ' + barrier)
-        print('Weight: ' + weight)
-        print('Penalty: ' + penalty)
-        print('Starting Price: ' + starting_price)
-
+        return saveRunnerDetails([finish_position, runner_number, runner_name, trainer_name,
+                                  jockey_name, margin_to_winner, barrier, weight, penalty,
+                                  starting_price], race_id, dbconnstr)
+        
     return 1
 
-def getRaceRunners(race_runners, race_number):
+def getRaceRunners(race_runners, race_id, dbconnstr):
     # iterate through runners
     runners_soup = bs4.BeautifulSoup(str(race_runners))
     for runners_row in runners_soup.findAll('tr'):
-        getRaceRunnerDetails(runners_row, race_number)
+        getRunnerDetails(runners_row, race_id, dbconnstr)
 
     return 1
 
-def getRaceMeetResults(pageURL, state):
+def getRaceMeetResults(pageURL, state, dbconnstr):
     # get the race meet details
     # prints for temporary debugging purposes
-    print(pageURL)
+    #print(pageURL)
 
     # get page specified in pageURL (replace spaces with %20 for correct url formatting)
     page_data = urllib.request.urlopen(pageURL.replace(' ', '%20') + '&BodyClass=PrintFriendly')
@@ -171,24 +162,24 @@ def getRaceMeetResults(pageURL, state):
 
     # get race meet details data
     meetDetails = getRaceLocationAndDate(pageURL, state)
-    getRaceMeetDetails(soup, meetDetails)
+    meet_id = getRaceMeetDetails(soup, meetDetails, dbconnstr)
 
     flag_runner_table_next = 0
-    race_number = ''
+    race_id = ''
 
     # iterate through race meet races
     for table_tag in soup.findAll('table'):
         # see if this table_tagle flagged as runners
         if flag_runner_table_next == 1:
             # get the runners for the race
-            getRaceRunners(table_tag, race_number)
+            getRaceRunners(table_tag, race_id, dbconnstr)
             # reset flag
             flag_runner_table_next = 0
 
         # see if the table is a race heading
         elif table_tag['class'] == ['race-title']:
             # get details of the race
-            race_number = getRaceDetails(table_tag)
+            race_id = getRaceDetails(table_tag, meet_id, dbconnstr)
             # flag the next table tag is going to be the runners for this race
             flag_runner_table_next = 1
 
@@ -210,8 +201,8 @@ for state in states:
         # ignore the risaform links
         if link.get('href') != 'http://www.risaform.com.au/Racing/default.aspx':
             # check if we've got this race meet's data yet
-            if needToGetRaceMeet('http://risa.com.au' + link.get('href'), state):
-                getRaceMeetResults('http://risa.com.au' + link.get('href'), state)
+            if needToGetRaceMeet('http://risa.com.au' + link.get('href'), state, dbconnstr):
+                getRaceMeetResults('http://risa.com.au' + link.get('href'), state, dbconnstr)
 
 
 
