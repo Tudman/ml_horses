eb0e8b47ee06f96d848b994376045779db91462b
Tudman <emptierchair@gmail.com>
2014-02-09 19:10:35 +1030

    finally have some preditctions.

--- analysis/anal.py
+++ analysis/anal.py
@@ -1,6 +1,8 @@
-#Importing library
+#importing libraries
 
 import psycopg2
+import numpy
+from sklearn import linear_model
 
 #establishing database connection details
 
@@ -13,38 +15,65 @@ def getConnection(dbconnstr):
 
 con = getConnection(dbconnstr)
 
-
+'''To do'''
 #write query defs...
 #get top ten horses by count in table
 #use as list and query the db, for name, time, distance, weight, track??? ... to get array
 #transform for numpy, and other ml.
 #get paramters and (create and) load them into a table
 
-''' get list of distinct horse names '''
-
 #sql = 'UPDATE stage.runner SET horse=lower(runner.horse);' # just making sure all the names are lowercase. comment this out if sure.
 
-sql = 'select distinct runner.horse from stage.runner'
+''' get list of distinct horse names, tucked away for a sec '''
+
+#sql = 'select distinct runner.horse from stage.runner'
+
+
+data_by_horse = []
+regress_parameters = []
 
 cur = con.cursor()
 
-cur.execute(sql)
-
-rows = cur.fetchall()
-
-''' test horse '''
-horse = 'acton girl'
-
-#row = (str(rows[0]).strip('"').strip('()')) cleans strings for passing back into db.
-
-
-sql = "SELECT runner.finish_time, runner.weight, race.racedistance, race.trackcondition, racemeeting.location" \
-    " FROM stage.runner, stage.race, stage.racemeeting WHERE runner.raceid = race.raceid" \
-    " AND race.racemeetingid =  racemeeting.racemeetingid AND runner.horse = '" + horse.replace("'","''") + "';"
+###''' test list '''###
+sql = 'select runner.horse, count(runner.horse) as count from stage.runner GROUP BY runner.horse ORDER BY count DESC LIMIT 1' 
 
 cur.execute(sql)
 
 rows = cur.fetchall()
 
 for row in rows:
-    print(row)
+    horse = (str(row[0]).strip('"')) # cleans strings for passing back into db.
+    sql = "SELECT runner.finish_time, runner.weight, race.racedistance, race.trackcondition, racemeeting.location" \
+    " FROM stage.runner, stage.race, stage.racemeeting WHERE runner.raceid = race.raceid" \
+    " AND race.racemeetingid =  racemeeting.racemeetingid AND runner.horse = '" + horse.replace("'","''") + "';"
+    cur.execute(sql)
+    race_rows = cur.fetchall()
+    data_race = []
+    for race_row in race_rows:
+       data_race.append(race_row)
+    data_by_horse.append((horse,data_race))   
+
+for i in range(len(data_by_horse)): #iterate throgh all the horses
+    horse = data_by_horse[i][0] #get horses name
+    x1 = []
+    x2 = []
+    y = []
+    for j in range(len(data_by_horse[0][1])): #iterate through the race data
+        y.append(float(data_by_horse[i][1][j][0]))
+        x1.append(float(data_by_horse[i][1][j][1]))
+        x2.append(int(data_by_horse[i][1][j][2][:-1:]))
+
+    y = numpy.array(y)
+    x1 = numpy.array(x1)
+    x2 = numpy.array(x2)
+    x = numpy.vstack([x1,x2]).T
+
+    print(len(x1),len(x2),len(y))
+    
+    regr = linear_model.LinearRegression()
+    regr.fit( x, y )
+    print(regr.predict([59.5, 1600]))
+       
+    #do regression stuff here and store the parameters.
+
+                       
