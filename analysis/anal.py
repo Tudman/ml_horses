3befd6ed726f499b90dba2a2e3798fa14aecde78
Tudman <emptierchair@gmail.com>
2014-02-09 13:55:29 +1030

    just recording some progress
    A bit of commit spam.

--- analysis/anal.py
+++ analysis/anal.py
@@ -22,6 +22,8 @@ con = getConnection(dbconnstr)
 
 ''' get list of distinct horse names '''
 
+#sql = 'UPDATE stage.runner SET horse=lower(runner.horse);' # just making sure all the names are lowercase. comment this out if sure.
+
 sql = 'select distinct runner.horse from stage.runner'
 
 cur = con.cursor()
@@ -30,7 +32,19 @@ cur.execute(sql)
 
 rows = cur.fetchall()
 
+''' test horse '''
+horse = 'acton girl'
+
+#row = (str(rows[0]).strip('"').strip('()')) cleans strings for passing back into db.
+
+
+sql = "SELECT runner.finish_time, runner.weight, race.racedistance, race.trackcondition, racemeeting.location" \
+    " FROM stage.runner, stage.race, stage.racemeeting WHERE runner.raceid = race.raceid" \
+    " AND race.racemeetingid =  racemeeting.racemeetingid AND runner.horse = '" + horse.replace("'","''") + "';"
+
+cur.execute(sql)
+
+rows = cur.fetchall()
+
 for row in rows:
     print(row)
-
-#sql = "SELECT stage.
