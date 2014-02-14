439c4cca63073062e624f507a19ddb3c25024c14
RogerMooreIsTheBestBond <tim.goldney@gmail.com>
2014-02-14 13:16:35 +1030

    Update notes.py

--- analysis/notes.py
+++ analysis/notes.py
@@ -3,3 +3,5 @@
 # stich together the data set (using the CSVs we've got). run multi-regression on horses.
 # then look at alternative machine learning techiques.
 # also, investigate Python/Django web framework. We need to make this biatch live somehow.
+
+# love your work dude, looking good!
